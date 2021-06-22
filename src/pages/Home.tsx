d7252d70c179eecd8e5665c1532ac8b4f38b186f
Hugo Matheus <programacao3@acade.com.br>
2021-06-22 14:05:41 -0300

    Criação e configuração da autenticação do sistema.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import { useHistory } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleInconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
+    history.push('/rooms/new');
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Imagem sobre perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo Lemmeask" />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleInconImg} alt="Logo Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entrer em uma sala</div>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
