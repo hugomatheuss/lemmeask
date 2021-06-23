e02dbb7b64ba7885011c9be40e2a0a79ac82bb03
Hugo Matheus <programacao3@acade.com.br>
2021-06-23 14:55:32 -0300

    Criando página para a sala de perguntas.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -7,11 +8,14 @@ import googleInconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 
+import { database } from '../services/firebase';
+
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
     if (!user) {
@@ -21,12 +25,29 @@ export function Home() {
     history.push('/rooms/new');
   }
 
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Imagem sobre perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo real</p>
+        <p>Tire as dÃºvidas da sua audiÃªncia em tempo real</p>
       </aside>
       <main>
         <div className="main-content">
@@ -36,10 +57,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entrer em uma sala</div>
-          <form>
-            <input 
-              type="text" 
-              placeholder="Digite o código da sala"
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o cÃ³digo da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
