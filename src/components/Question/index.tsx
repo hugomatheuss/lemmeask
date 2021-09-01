ff87e6a1f96c1505abd26d8d81efd6245ff7021a
Hugo Matheus <programacao3@acade.com.br>
2021-09-01 11:57:29 -0300

    Implementando a lógica dos likes nas perguntas.

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,16 +1,20 @@
+import { ReactNode } from 'react';
+
+import './styles.scss';
+
 type QuestionProps = {
   content: string;
   author: {
     name: string;
     avatar: string;
   }
+  children?: ReactNode;
 }
-
-import './styles.scss';
 
 export function Question({
   content,
   author,
+  children,
 }: QuestionProps) {
   return (
     <div className="question">
@@ -19,6 +23,9 @@ export function Question({
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
+        </div>
+        <div>
+          {children}
         </div>
       </footer>
     </div>
